710f62599c9cbbd368d9236a5fee40625e95a88d
aletbm <alexanderdaniel_rios@hotmail.com>
2022-07-23 12:23:10 -0300

    Fixed errors

--- js/nav_search.js
+++ js/nav_search.js
@@ -1,30 +1,30 @@
 import { fetchData } from "../services/getData.js"
 
-document.getElementsByClassName("header-nav-lupa")[0].addEventListener("click",() => {
+document.getElementsByClassName("header-nav-lupa")[0].addEventListener("click", () => {
     document.getElementById('form-buscador').classList.toggle('no-visible')
 })
 
-document.getElementsByClassName("header-nav-lupa")[1].addEventListener("click",() => {
+document.getElementsByClassName("header-nav-lupa")[1].addEventListener("click", () => {
     document.getElementById('form-buscador-mobile').classList.toggle('no-visible')
 })
 
-document.getElementById('btnMobile').addEventListener("click", function(){
+document.getElementById('btnMobile').addEventListener("click", function () {
     document.getElementById('linksMobile').classList.toggle('no-visible')
 })
 
-document.getElementsByClassName('carrito')[0].addEventListener('click', function(){
+document.getElementsByClassName('carrito')[0].addEventListener('click', function () {
     window.location.href = "./carrito.html"
 })
 
-document.getElementsByClassName('carrito')[1].addEventListener('click', function(){
+document.getElementsByClassName('carrito')[1].addEventListener('click', function () {
     window.location.href = "./carrito.html"
 })
 
-function updateCounter(){
+function updateCounter() {
     const p = document.getElementsByClassName('carrito')[0].childNodes[3]
     const p_mobile = document.getElementsByClassName('carrito')[1].childNodes[3]
     let prds = localStorage.length
-    if(localStorage.getItem("uid")){
+    if (localStorage.getItem("uid")) {
         prds -= 1
     }
     p.innerHTML = prds
@@ -33,22 +33,22 @@ function updateCounter(){
 
 window.setInterval(updateCounter, 1000)
 
-function userSession(){
+function userSession() {
     fetchData("/admin").then((response) => {
         const uid = localStorage.getItem("uid")
-        if(uid == response.uid){
-           const btn = document.querySelector('[data-login]')
-           const btnMobile = document.querySelector('[data-loginMobile]')
-           btn.innerHTML = "CERRAR SESION"
-           btn.href = "./index.html"
-           btn.addEventListener("click", () => {
-            localStorage.removeItem("uid")
-           })
-           btnMobile.innerHTML = "CERRAR SESION"
-           btnMobile.href = "./index.html"
-           btnMobile.addEventListener("click", () => {
-            localStorage.removeItem("uid")
-           })
+        if (uid == response.uid) {
+            const btn = document.querySelector('[data-login]')
+            const btnMobile = document.querySelector('[data-loginMobile]')
+            btn.innerHTML = "CERRAR SESION"
+            btn.href = "./index.html"
+            btn.addEventListener("click", () => {
+                localStorage.removeItem("uid")
+            })
+            btnMobile.innerHTML = "CERRAR SESION"
+            btnMobile.href = "./index.html"
+            btnMobile.addEventListener("click", () => {
+                localStorage.removeItem("uid")
+            })
         }
         return true
     }).catch(function (error) {
@@ -58,20 +58,20 @@ function userSession(){
 
 userSession()
 
-document.getElementById('form-buscador').addEventListener('submit', function(event){
+document.getElementById('form-buscador').addEventListener('submit', function (event) {
     event.preventDefault()
     const busqueda = document.getElementById("buscador").value
-    if(busqueda.replace(/ /g, "").length == 0 || !/^[a-zA-Z0-9 ]*$/.test(busqueda)) return
-    else{
-        window.location.href = "./busquedas.html?searchName="+busqueda
+    if (busqueda.replace(/ /g, "").length == 0 || !/^[a-zA-Z0-9 ]*$/.test(busqueda)) return
+    else {
+        window.location.href = "./busquedas.html?searchName=" + busqueda
     }
 })
 
-document.getElementById('form-buscador.mobile').addEventListener('submit', function(event){
+document.getElementById('form-buscador-mobile').addEventListener('submit', function (event) {
     event.preventDefault()
     const busqueda = document.getElementById("buscador-mobile").value
-    if(busqueda.replace(/ /g, "").length == 0 || !/^[a-zA-Z0-9 ]*$/.test(busqueda)) return
-    else{
-        window.location.href = "./busquedas.html?searchName="+busqueda
+    if (busqueda.replace(/ /g, "").length == 0 || !/^[a-zA-Z0-9 ]*$/.test(busqueda)) return
+    else {
+        window.location.href = "./busquedas.html?searchName=" + busqueda
     }
 })
